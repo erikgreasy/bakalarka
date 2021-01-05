f198360bd6ae05adcac5232d9155295e594c7a28
erik.masny <erik.masny@gmail.com>
2021-01-05 22:23:51 +0100

    Added manifest link, fonts and commented out tiny mce editor script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,71 +14,26 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="manifest" href="/js/manifest.json">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        @auth
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-
-                        
-                            <li class="nav-item">
-                                <a href="/" class="nav-link">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/hills" class="nav-link">Hills</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/users" class="nav-link">Users</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/my-profile" class="nav-link">My profile</a>
-                            </li>
-                            
-                            <li class="nav-item">
-                                <a class="nav-link text-danger" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        @endauth
         
+        <main>
 
-        <main class="py-4">
-            <div class="container">
-
-                @yield('content')
-            </div>
+            @yield('content')
         </main>
+        @auth
+            @include( 'partials.navigation' )
+        @endauth
     </div>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>  
-    <script type="text/javascript">
+    {{-- <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>   --}}
+    {{-- <script type="text/javascript">
   tinymce.init({
   selector: 'textarea.tinymce-editor',
   height: 500,
@@ -94,6 +49,6 @@
   'removeformat | help',
   content_css: '//www.tiny.cloud/css/codepen.min.css'
 });
-    </script>  
+    </script>   --}}
 </body>
 </html>
