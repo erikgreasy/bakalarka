0f4646e89264fb07a0ae7143022aeb4f2d153e78
erik.masny <erik.masny@gmail.com>
2021-01-06 19:34:32 +0100

    Added mobile navigation bar

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,16 @@
+<nav class="mobile-navigation-bar">
+    <ul>
+        <li>
+            <a href="/"><i class="fas fa-home"></i></a>
+        </li>
+        <li>
+            <a href="/hills"><i class="fas fa-mountain"></i></a>
+        </li>
+        <li>
+            <a href="/users"><i class="fas fa-user-friends"></i></a>
+        </li>
+        <li>
+            <a href="/my-profile"><i class="fas fa-user"></i></a>
+        </li>
+    </ul>
+</nav>
