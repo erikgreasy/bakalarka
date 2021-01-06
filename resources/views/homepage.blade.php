84b7acf7745c593972dfa37b969b14e1efd5e282
erik.masny <erik.masny@gmail.com>
2021-01-06 21:44:32 +0100

    Changed logged homepage template a little bit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    This is homepage
+    <div>
+        <h1>Ahoj Erik</h1>
+        <p>Kam sa vydáš dnes na dobrodružstvo?</p>
+    </div>
+    <div>
+        <h2>Moje kopce</h2>
+        <a href="#">Zobraziť všetky</a>
+    </div>
+    <div>
+        <h2>Najnovšie dobrodružstvá</h2>
+        <a href="#">Zobraziť všetky</a>
+    </div>
+    <div>
+        <h2>Rebríček dobrodruhov</h2>
+        <a href="#">Zobraziť všetky</a>
+    </div>
 
 
 @endsection
