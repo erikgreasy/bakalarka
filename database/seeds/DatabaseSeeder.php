4955d0b23a0a6e6bcb6713c03364197a16d5a9fc
erik.masny <erik.masny@gmail.com>
2021-04-11 01:03:47 +0200

    Added trip seeder to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call(MountainSeeder::class);
         $this->call(HillSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(TripSeeder::class);
+
 
 
     }
