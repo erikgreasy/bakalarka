d1c62e039c3d6058614fa9cfab90667f0ea2767e
erik.masny <erik.masny@gmail.com>
2021-04-13 13:14:08 +0200

    Created float btn component

---
+++ resources/js/components/FloatBtn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="floating-btn" @click="open">
+            <i class="fas fa-th"></i>
+        </div>
+        <ul class="floating-btn-options">
+            <li>Link1</li>
+            <li>Link2</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+    methods: {
+        open() {
+            alert('open')
+        }
+    }
+    
+}
+</script>
