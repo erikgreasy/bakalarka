c9f1f70a3ef21aa694fa129169b614177245d955
erik.masny <erik.masny@gmail.com>
2021-05-09 19:25:57 +0200

    Uncommented generating and adding SW

--- resources/js/app.js
+++ resources/js/app.js
@@ -127,16 +127,16 @@ const app = new Vue({
 /**
  * REGISTER SERVICE WORKER
  */
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//             // Registration was successful
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }, function(err) {
-//             // registration failed :(
-//             console.error('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// } else {
-//     console.log('service worker not working')
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.error('ServiceWorker registration failed: ', err);
+        });
+    });
+} else {
+    console.log('service worker not working')
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,20 +15,20 @@ require('laravel-mix-workbox');
 
 mix.js('resources/js/app.js', 'public/js').sourceMaps()
     .sass('resources/sass/app.scss', 'public/css')
-    // .injectManifest({
-    //     swSrc: './resources/js/service-worker.js',
-    //     swDest: 'sw.js',
-    //     maximumFileSizeToCacheInBytes: 50000000,
-    //     additionalManifestEntries: [
-    //         {url: '/images/image-placeholder.png', revision: null},
-    //         {url: '/images/default.png', revision: null},
-    //         {url: '/images/offline.png', revision: null},
-    //         {url: '/images/mountain-bg.jpg', revision: null},
-    //         {url: '/manifest.json', revision: null},
-    //         {url: '/icon/turista-icon.png', revision: null},
-    //         {url: '/icon/turista-512x512.png', revision: null},
-    //         {url: '/icon/maskable_icon_x192.png', revision: null},
-    //         {url: '/favicon.ico', revision: null},
+    .injectManifest({
+        swSrc: './resources/js/service-worker.js',
+        swDest: 'sw.js',
+        maximumFileSizeToCacheInBytes: 50000000,
+        additionalManifestEntries: [
+            {url: '/images/image-placeholder.png', revision: null},
+            {url: '/images/default.png', revision: null},
+            {url: '/images/offline.png', revision: null},
+            {url: '/images/mountain-bg.jpg', revision: null},
+            {url: '/manifest.json', revision: null},
+            {url: '/icon/turista-icon.png', revision: null},
+            {url: '/icon/turista-512x512.png', revision: null},
+            {url: '/icon/maskable_icon_x192.png', revision: null},
+            {url: '/favicon.ico', revision: null},
 
-    //     ]
-    // });
+        ]
+    });
